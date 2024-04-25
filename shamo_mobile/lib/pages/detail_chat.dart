ddd8fa33f18ea6eb700f31753ea6d859f2b8af32
rekto167 <ekysr99@gmail.com>
2024-04-25 08:45:33 +0700

    Chat Input 2 Product Preview

--- shamo_mobile/lib/pages/detail_chat.dart
+++ shamo_mobile/lib/pages/detail_chat.dart
@@ -1,3 +1,4 @@
+import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import "package:flutter/widgets.dart";
 import "package:shamo_mobile/theme.dart";
@@ -43,30 +44,91 @@ class DetailChat extends StatelessWidget {
       );
     }
 
-
-    Widget chatInput(){
+    Widget productPreview() {
       return Container(
-        margin: EdgeInsets.all(20),
+        width: 225,
+        height: 74,
+        margin: EdgeInsets.only(bottom: 20),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: bgColor5,
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: primaryColor)),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset(
+                  'assets/image_shoes_running.png',
+                  width: 54,
+                )),
+            SizedBox(
+              width: 10,
+            ),
             Expanded(
-              child: Container(
-                height: 45,
-                padding: EdgeInsets.symmetric(horizontal: 16,),
-                decoration: BoxDecoration(
-                  color: bgColor4,
-                  borderRadius: BorderRadius.circular(12)
-                ),
-                child: Center(child: TextFormField(
-                  decoration: InputDecoration.collapsed(
-                    hintText: 'Type Message...',
-                    hintStyle: subtitleTextStyle
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'COURT VISIO...',
+                    style: primaryTextStyle,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                ),),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    '\$57,15',
+                    style: priceTextStyle.copyWith(fontWeight: medium),
+                  ),
+                ],
               ),
             ),
-            SizedBox(width: 20,),
-            Image.asset('assets/button_send.png', width: 45,)
+            Image.asset('assets/button_close.png', width: 22,)
+          ],
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            productPreview(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    decoration: BoxDecoration(
+                        color: bgColor4,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Center(
+                      child: TextFormField(
+                        decoration: InputDecoration.collapsed(
+                            hintText: 'Type Message...',
+                            hintStyle: subtitleTextStyle),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Image.asset(
+                  'assets/button_send.png',
+                  width: 45,
+                )
+              ],
+            ),
           ],
         ),
       );
